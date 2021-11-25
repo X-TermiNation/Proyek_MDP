5189f1f0ae40483189a8195c9eb6ad84f36bb3a2
Hao <kevinoctaviushao@gmail.com>
2021-11-25 19:12:40 +0700

    Sync login + register

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,12 +2,20 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 
 import com.example.myapplication.databinding.ActivityLoginBinding;
 import com.example.myapplication.databinding.ActivityMainBinding;
+
+import java.lang.ref.WeakReference;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -28,5 +36,59 @@ public class LoginActivity extends AppCompatActivity {
         });
 
 
+    }
+}
+
+class LoginSync{
+    private final WeakReference<Context> weakContext;
+    private final WeakReference<loginCallback> weakCallback;
+    private String Username;
+    private String password;
+    public LoginSync(String username, String password,Context context, loginCallback callback) {
+        this.Username = username;
+        this.password = password;
+        this.weakContext = new WeakReference<>(context);
+        this.weakCallback = new WeakReference<>(callback);
+    }
+    Boolean IsFound = false;
+    void execute(){
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+        weakCallback.get().preExecute();
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                Context context = weakContext.get();
+                UserDatabase userDatabase = UserDatabase.getUserDatabase(context);
+                List<User> listUser;
+                listUser = userDatabase.userDao().gettAllUser();
+                for (int i = 0; i < listUser.size(); i++) {
+                    if(Username.equals(listUser.get(i).getEmail())){
+                        if(password.equals(listUser.get(i).getPassword())){
+                            IsFound = true;
+                        }
+                    }
+                }
+                if(IsFound){
+                    handler.post(()->{
+                        String succesMessage ="Login Berhasil";
+                        weakCallback.get().postExecute(succesMessage);
+                    });
+                    IsFound = false;
+                }else{
+                    handler.post(()->{
+                        String succesMessage ="Login Gagal";
+                        weakCallback.get().postExecute(succesMessage);
+                    });
+                }
+
+            }
+        });
+    }
+
+
+    interface loginCallback{
+        void preExecute();
+        void postExecute(String message);
     }
 }
--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -2,9 +2,17 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import com.example.myapplication.databinding.ActivityRegisterBinding;
+
+import java.lang.ref.WeakReference;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class RegisterActivity extends AppCompatActivity {
     private ActivityRegisterBinding binding;
@@ -14,5 +22,55 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         binding = ActivityRegisterBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
+}
+
+class AddUserSync{
+    private final WeakReference<Context> weakContext;
+    private final WeakReference<AddUpdateNoteCallback> weakCallback;
+    private User user;
+
+    public AddUserSync(User user,Context context, AddUpdateNoteCallback callback) {
+        this.weakContext = new WeakReference<>(context);
+        this.weakCallback = new WeakReference<>(callback);
+        this.user = user;
+    }
+    Boolean found = false;
+    void execute(){
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+        weakCallback.get().preExecute();
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                Context context = weakContext.get();
+                UserDatabase userDatabase = UserDatabase.getUserDatabase(context);
+                List<User> listUser;
+                listUser = userDatabase.userDao().gettAllUser();
+                for (int i = 0; i < listUser.size(); i++) {
+                    if(user.getEmail().matches(listUser.get(i).getEmail())){
+                        found = true;
+                    }
+                }
+                if (found){
+                    handler.post(()->{
+                        String succesMessage ="User Sudah Terdaftar";
+                        weakCallback.get().postExecute(succesMessage);
+                    });
+                }else{
+                    userDatabase.userDao().insertUser(user);
+                    handler.post(()->{
+                        String succesMessage ="New User Added";
+                        weakCallback.get().postExecute(succesMessage);
+                    });
+                }
+            }
+        });
+    }
+
+
+    interface AddUpdateNoteCallback{
+        void preExecute();
+        void postExecute(String message);
     }
 }
